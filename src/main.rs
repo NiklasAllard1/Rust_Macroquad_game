a367b5f0c70581730e87631ad3b6323d89e3d581
NiklasAllard1 <143690578+NiklasAllard1@users.noreply.github.com>
2024-01-03 18:41:36 +0100

    Added moveable circle

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,25 @@ use macroquad::prelude::*;
 
 #[macroquad::main("Mitt spel")]
 async fn main() {
+    let mut x = screen_width() / 2.0;
+    let mut y = screen_height() /2.0;
     loop {
         clear_background(DARKBLUE);
+        if is_key_down(KeyCode::Right) {
+            x += 2.0;
+        }
+        if is_key_down(KeyCode::Left) {
+            x -= 2.0;
+        }
+        if is_key_down(KeyCode::Down) {
+            y += 2.0;
+        }
+        if is_key_down(KeyCode::Up) {
+            y -= 2.0;
+        }
+
+        draw_circle(x, y, 18.0, RED);
+
         next_frame().await
     }
 }
