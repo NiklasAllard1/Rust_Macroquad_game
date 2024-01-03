eb3a04e2dfe0eced60954620512a658f38c9d88f
NiklasAllard1 <143690578+NiklasAllard1@users.noreply.github.com>
2024-01-03 17:55:31 +0100

    Fixed new color

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use macroquad::prelude::*;
 #[macroquad::main("Mitt spel")]
 async fn main() {
     loop {
-        clear_background(DARKPURPLE);
+        clear_background(DARKBLUE);
         next_frame().await
     }
 }
